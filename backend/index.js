4d88e0823660b14fa013a1c572618c54ccaa2fdb
nafi-ullah <shafinnafiullah@gmail.com>
2024-03-20 23:14:12 +0600

    backend initial

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,30 @@
-console.log("hellow word");
+const express = require("express");
+
+const mydatabase = require("mongoose");
+
+const app = express();
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+   res.header("Access-Control-Allow-Headers", "Content-Type");
+   next();
+ });
+
+
+const PORT = 5000;
+const DB =
+  "mongodb+srv://sweboy:dmc54321@cluster0.trbboqn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+
+mydatabase
+  .connect(DB)
+  .then(() => {
+    console.log("Daatabase Connected successfully");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+const server = app.listen(PORT, () => {
+  console.log(`Server is running  on ${PORT}`);
+});
