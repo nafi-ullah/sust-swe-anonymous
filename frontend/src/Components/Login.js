c1d48ad9dca843d5899d6d2e85baf285ec743a5c
Nixon Deb Antu <nixondebantu@gmail.com>
2024-03-21 23:11:55 +0600

    placeholder update

--- frontend/src/Components/Login.js
+++ frontend/src/Components/Login.js
@@ -39,7 +39,7 @@ const Login = ({ loggedIn, onLogin, error }) => {
                 autoComplete="username"
                 required
                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                placeholder="Username"
+                placeholder="Join"
                 value={username}
                 onChange={(e) => setUsername(e.target.value)}
               />
